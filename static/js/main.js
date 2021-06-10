cf0fab9cc4b51a67a7223dc4eb2fe94e2f48e83a
Luisa <lu_rfa@hotmail.com>
2021-06-09 23:21:11 -0300

    Added JS file handler

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,31 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-   Module.onRuntimeInitialized = async _ => {
-     let tk = new verovio.toolkit();
-     console.log("Verovio has loaded!");
+  Module.onRuntimeInitialized = async _ => {
+    let tk = new verovio.toolkit();
+    console.log("Verovio has loaded!");
+    
+    if(typeof fileList !== 'undefined')
+      fetch(fileList[0])
+    else
+      fetch("https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei")
+      
+    .then( (response) => response.text() )
+    .then( (meiXML) => {
+        let svg = tk.renderData(meiXML, {});
+        document.getElementById("rendering").innerHTML = svg;
+      });
 
-     fetch("https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei")
-       .then( (response) => response.text() )
-       .then( (meiXML) => {
-         let svg = tk.renderData(meiXML, {});
-         document.getElementById("rendering").innerHTML = svg;
-       });
-
-       tk.setOptions({
-         scale: 35,
-         portrait: true,
-         adjustPageWidth: true
-       });
-   }
+    tk.setOptions({
+        scale: 35,
+        portrait: true,
+        adjustPageWidth: true
+    });
+  }
 });
+
+const inputElement = document.getElementById("input");
+
+inputElement.addEventListener("change", handleFiles, false);
+
+function handleFiles() {
+  const fileList = this.files; /* now you can work with the file list */
+}
