efbdd69d49ac043ec887cc2b7a2828682e77279a
Luisa <lu_rfa@hotmail.com>
2021-06-18 17:35:22 -0300

    Tests

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
 
+##app name
 app = Flask(__name__, template_folder='templates')
 
+##encryption key
+app.secret_key = "banane"
+
+##absolute path
+path = os.getcwd()
+UPLOAD_FOLDER = os.path.join(path, 'random_files/uploads')
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
-@app.route('/inicio')
-def init():
+def index():
     return render_template('index.html', titulo="ApolloScores")
+
+@app.route('/', methods=['GET','POST'])
+def upload_file():
+    print("It gets here")
+    if request.method == 'POST':
+    
+        file = request.files['file']
+        print(file.filename)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    return redirect('/')
+
+
 
 if __name__ == '__main__':
     app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,63 @@
+/*
+<!-- Score.vue -->
+<template>
+    <div v-html="renderedScore"></div>
+</template>
+
+<script>
+
+*/
+/*
+import verovio from 'verovio'; // load verovio as a webpack external
+
+export default {
+    data() {
+        return {
+            verovioToolkit: null,
+            renderedScore: null,
+            scale: 40,
+            landscape: false,
+            page: 1,
+        };
+    },
+    created() {
+        this.verovioToolkit = new verovio.toolkit();
+    },
+    mounted() {
+        this.setVerovioOptions();
+        this.loadScoreFile('/path/to/score.mei');
+    },
+    watch: {
+        scale() {
+            this.rerenderCurrentView();
+        },
+        page(value) {
+            this.rerenderCurrentView();
+        },
+    },
+    methods: {
+        setVerovioOptions() {
+            this.verovioToolkit.setOptions({
+                scale: this.scale,
+                noHeader: true,
+                noFooter: true,
+                landscape: false,
+                pageWidth: this.width * (100 / this.scale),
+                pageHeight: this.height * (100 / this.scale),
+            });
+        },
+        async loadScoreFile(path) {
+            const response = await fetch(path);
+            const data = await response.text();
+            this.renderedScore = this.verovioToolkit.renderData(data, {});
+        },
+        rerenderCurrentView() {
+            this.setVerovioOptions();
+            this.verovioToolkit.redoLayout();
+            this.renderedScore = this.verovioToolkit.renderToSVG(this.page, {});
+        },
+    },
+};*/
 function handleFiles() {
   const fileList = this.files; /* now you can work with the file list */
   
